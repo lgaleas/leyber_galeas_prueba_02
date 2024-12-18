2dca066556e9818e4c204b75d775378a29ee732b
Leyber Galeas <169060193+lgaleas@users.noreply.github.com>
2024-12-18 18:17:29 -0500

    LEYBER GALEAS 18h17

--- app/src/main/java/com/leyber/galeas/leyber_galeas_prueba_02/MainActivity.java
+++ app/src/main/java/com/leyber/galeas/leyber_galeas_prueba_02/MainActivity.java
@@ -1,6 +1,9 @@
 package com.leyber.galeas.leyber_galeas_prueba_02;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,26 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText etNombresU, etApellidosU, etDividendoU, etDivisorU, etParteEnteraU, etResiduoU, etInvertidoU;
+    private Button btnSiguienteU, btnMostrarResultadosU;
+    private boolean regresoConDatos = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.layout_activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        etNombresU = findViewById(R.id.etNombresU);
+        etApellidosU = findViewById(R.id.etApellidosU);
+        etDividendoU = findViewById(R.id.etDividendoU);
+        etDivisorU = findViewById(R.id.etDivisorU);
+        etParteEnteraU = findViewById(R.id.etParteEnteraU);
+        etResiduoU = findViewById(R.id.etResiduoU);
+        etInvertidoU = findViewById(R.id.etInvertidoU);
+
+        
+
     }
+
 }
